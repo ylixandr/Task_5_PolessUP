eddce811baf85d4568cf86f2ee315185cd4cd717
ilya <User@WIN-ISS673VO7OO>
2023-04-18 10:46:51 +0300

    Добавьте файлы проекта.

---
+++ Task_5_PolessUP/Program.cs
@@ -0,0 +1,26 @@
+﻿//Дан массив целых чисел (неупорядоченный). Реализуйте алгоритм, который находит элемент с максимальным значением. Не используйте LINQ. 
+int[] array = { 1, 4, 2, 7, 3, 9, 0, -1 };
+
+
+static int IndexOfMAx(int[] array)
+{
+    int max = array[0];
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        { max = array[i]; }
+    }
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == max)
+        {
+            index = i; break;
+        }
+
+    }
+    return index;
+}
+
+Console.WriteLine(IndexOfMAx(array));
